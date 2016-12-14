fa37c328ddd1a037107779603a911c2277d889e1
optikfluffel <optik@fluffel.io>
2016-12-14 22:15:58 +0100

    add pdf download + browsersync for development

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 const gulp = require('gulp');
 const clean = require('gulp-clean');
+const rename = require('gulp-rename');
 const hb = require('gulp-hb');
 const sass = require('gulp-sass');
 const watch = require('gulp-watch');
 const htmlmin = require('gulp-htmlmin');
+const browserSync = require('browser-sync').create(); // create a browser sync instance.
 
 // ------------------------------ CLEAN ------------------------------
 gulp.task('clean:handlebars', function() {
@@ -22,11 +24,12 @@ gulp.task('clean:images', function() {
 // ------------------------------ BUILD ------------------------------
 gulp.task('handlebars', ['clean:handlebars'], function() {
   return gulp
-    .src('src/*.html')
+    .src('src/index.hbs')
     .pipe(hb({
       partials: 'src/partials/**/*.hbs',
     }))
     .pipe(htmlmin())
+    .pipe(rename({extname: '.html'}))
     .pipe(gulp.dest('docs'));
 });
 
@@ -45,14 +48,32 @@ gulp.task('images', ['clean:images'], function() {
 gulp.task('handlebars:watch', ['handlebars'], function () {
   gulp.watch(['src/*.html', 'src/partials/**/*.hbs'], ['handlebars']);
 });
+
 gulp.task('sass:watch', ['sass'], function () {
   gulp.watch('src/scss/**/*.scss', ['sass']);
 });
+
 gulp.task('images:watch', ['images'], function () {
   gulp.watch('src/images/**/*', ['images']);
 });
 
+// ------------------------------- DEV -------------------------------
+gulp.task('browser-sync', ['watch'], function() {
+    browserSync.init({
+        server: {
+            baseDir: "./docs"
+        }
+    });
+
+    gulp.watch('docs/**/*', ['reload'])
+});
+
+gulp.task('reload', function(done) {
+  browserSync.reload();
+  done();
+})
+
 // ------------------------------ TASKS ------------------------------
 gulp.task('build', ['handlebars', 'sass', 'images']);
 gulp.task('watch', ['handlebars:watch', 'sass:watch', 'images:watch']);
-gulp.task('default', ['watch']);
+gulp.task('default', ['watch', 'browser-sync']);
